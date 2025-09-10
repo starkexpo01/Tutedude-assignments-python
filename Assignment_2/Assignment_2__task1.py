81275b9bd0bffc9288283f8fc3b7787bf406b0ec
stakrexpo01 <bavneet.official@gmail.com>
2025-09-10 09:58:40 +0530

    assignment_2

---
+++ Assignment_2/Assignment_2__task1.py
@@ -0,0 +1,5 @@
+skj = int(input("Enterthe number: "))
+if (skj % 2 == 0):
+    print(skj,"is an even number")
+else:
+    print(skj,"is an odd number")
---
+++ Assignment_2/Assignment_2__task2.py
@@ -0,0 +1,4 @@
+skj = 0
+for bsj in range (1,51):
+    skj = skj + bsj
+print("The sum of numbers from 1 to 50 is:", skj)
